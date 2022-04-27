817be71630c7e08180cd5c2a019af2cebe5c83ed
MielkeDaniel <mielkedani@outlook.de>
2022-04-27 15:24:25 +0200

    initial commit

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerJump : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    
+    private Vector3 playerVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+    private bool isGrounded;
+    public float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+    private bool jumpPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        MovementJump();
+    }
+
+    void MovementJump() {
+        isGrounded = true;
+
+        // If on the ground, stop vertical movement
+
+
+        if (jumpPressed && isGrounded) {
+            Debug.Log("Geht rein bruda");
+            playerVelocity.y += jumpHeight;
+            rb.AddForce(new Vector3(0.0f, 900.0f, 0.0f));
+            jumpPressed = false;
+        }
+
+    }
+
+    void OnJump() {
+        if (rb.velocity.y == 0) {
+            jumpPressed = true;
+        } 
+    }
+}
