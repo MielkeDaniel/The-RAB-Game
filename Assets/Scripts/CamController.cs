506ce8c5ece0b89d8dc11eead4864eeff9673b97
MielkeDaniel <mielkedani@outlook.de>
2022-05-04 15:52:12 +0200

    cam rotation

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class CamController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
-
-    [Header("Framing")]
-    [SerializeField] private Camera cam;
-    [SerializeField] private Transform followTransform; 
-
+ 
+    private const float YMin = -50.0f;
+    private const float YMax = 50.0f;
+ 
+    public Transform lookAt;
+ 
+    public Transform Player;
+ 
+    public float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity = 4.0f;
+ 
+ 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position * 1.5f - player.transform.position;
+     
+ 
     }
-
+ 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+ 
+        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        currentY += Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+ 
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+ 
+        Vector3 Direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * Direction * -1;
+ 
+        transform.LookAt(lookAt.position);
     }
 }
+ 
